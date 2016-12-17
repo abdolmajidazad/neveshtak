f0d47dc2aaf565ea687b644c9cfa64ce89b6e472
martin <am.azad66@gmail.com>
2016-12-17 12:09:15 +0330

    create tags tab

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -39,6 +39,10 @@ export class SchedulePage {
   selectedCat = '';
   json = {};
   titleCat = '';
+  endLevelData = '';
+  endLevelDataStatus = false;
+  tagSelected ='';
+  subTag = false;
 
   constructor(
     public alertCtrl: AlertController,
@@ -58,6 +62,21 @@ export class SchedulePage {
   }
 
 
+  tagsSet(){
+    this.endLevelDataStatus = false;
+    this.subCat = 'hide';
+    this.subTag = false;
+  }
+  categorySet(){
+    this.endLevelDataStatus = false;
+    this.subCat = 'hide';
+    this.subTag = false;
+  }
+  userSet(){
+    this.endLevelDataStatus = false;
+    this.subCat = 'hide';
+    this.subTag = false;
+  }
   updateSchedule(data) {
     console.log(data)
     this.segment = data
@@ -77,6 +96,54 @@ export class SchedulePage {
     //  this.shownSessions = data.shownSessions;
     //  this.groups = data.groups;
     //});
+  }
+
+
+  public tagsData= [
+    {
+      'id' :2,
+      'title' : 'Hashtag 1',
+      'image' : 'assets/img/speakers/bear.jpg',
+      'description' : "6 hour age",
+      'count' : 634,
+      'owner' : 'apz',
+      'imageOwner' : 'assets/img/speakers/andrew.jpg'
+    },
+    {
+      'id' :3,
+      'title' : 'Hashtag 2',
+      'image' : 'assets/img/speakers/cheetah.jpg',
+      'description' : "5 hour age",
+      'count' : 1003,
+      'owner' : 'AMA',
+      'imageOwner' : 'assets/img/speakers/majid.jpg'
+    },
+    {
+      'id' :4,
+      'title' : 'Hashtag 3',
+      'image' : 'assets/img/speakers/iguana.jpg',
+      'description' : "9 hour age",
+      'count' : 5323,
+      'owner' : 'Srh',
+      'imageOwner' : 'assets/img/speakers/ray.jpg'
+    },{
+      'id' :9,
+      'title' : 'Hashtag 4',
+      'image' : 'assets/img/speakers/cheetah.jpg',
+      'description' : "yesterday",
+      'count' :32,
+      'owner' : 'mgs',
+      'imageOwner' : 'assets/img/speakers/steve.jpg'
+    }
+  ];
+
+  getTagData(data){
+    this.tagSelected = data;
+    this.subTag = true;
+  }
+
+  subTagBack(){
+    this.subTag = false;
   }
   //
   //presentFilter() {
@@ -172,73 +239,121 @@ export class SchedulePage {
   }
 
   subCategory(status, data, title){
-    console.log("mmamamamamamma")
+    this.subTag = false;
+    console.log("status, data, title : ", status, data, title)
     this.subCat = status;
     this.titleCat = title;
+    this.endLevelDataStatus = false;
 
     this.json = {
       cat1 : [
         {
+          'id' :1,
+          'category' : 'cat1',
           'title' : 'Category 1.1',
           'image' : 'assets/img/speakers/eagle.jpg',
-          'description' : "Upload 4 hour age by apz",
-          'count' : 54
+          'description' : "4 hour age",
+          'count' : 54,
+          'owner' : 'apz',
+          'imageOwner' : 'assets/img/speakers/andrew.jpg'
+
         },
         {
+          'id' :2,
+          'category' : 'cat1',
           'title' : 'Category 1.2',
           'image' : 'assets/img/speakers/bear.jpg',
-          'description' : "Upload 6 hour age by apz",
-          'count' : 634
+          'description' : "6 hour age",
+          'count' : 634,
+          'owner' : 'apz',
+          'imageOwner' : 'assets/img/speakers/andrew.jpg'
         }
       ],
       cat2 : [
         {
+          'category' : 'cat2',
+          'id' :3,
           'title' : 'Category 2.1',
           'image' : 'assets/img/speakers/cheetah.jpg',
-          'description' : "Upload 5 hour age by ama",
-          'count' : 1003
+          'description' : "5 hour age",
+          'count' : 1003,
+          'owner' : 'AMA',
+          'imageOwner' : 'assets/img/speakers/majid.jpg'
         },
         {
+          'category' : 'cat2',
+          'id' :4,
           'title' : 'Category 2.2',
           'image' : 'assets/img/speakers/puppy.jpg',
-          'description' : "Upload 7 hour age by ama",
-          'count' : 123
+          'description' : "7 hour age",
+          'count' : 123,
+          'owner' : 'AMA',
+          'imageOwner' : 'assets/img/speakers/majid.jpg'
         },
         {
+          'category' : 'cat2',
+          'id' :5,
           'title' : 'Category 2.2',
           'image' : 'assets/img/speakers/rabbit.jpg',
-          'description' : "Upload yesterday by apz",
-          'count' : 5323
+          'description' : "yesterday",
+          'count' : 5323,
+          'owner' : 'apz',
+          'imageOwner' : 'assets/img/speakers/andrew.jpg'
         }
       ],
       cat3 : [
         {
+          'category' : 'cat3',
+          'id' :6,
           'title' : 'Category 3.1',
           'image' : 'assets/img/speakers/turtle.jpg',
-          'description' : "Upload 4 hour age by srh",
-          'count' : 45
+          'description' : "4 hour age",
+          'count' : 45,
+          'owner' : 'Srh',
+          'imageOwner' : 'assets/img/speakers/ray.jpg'
         },
         {
+          'category' : 'cat3',
+          'id' :7,
           'title' : 'Category 3.2',
           'image' : 'assets/img/speakers/iguana.jpg',
-          'description' : "Upload 9 hour age by srh",
-          'count' : 5323
+          'description' : "9 hour age",
+          'count' : 5323,
+          'owner' : 'Srh',
+          'imageOwner' : 'assets/img/speakers/ray.jpg'
         }
       ],
       cat4 : [
         {
+          'category' : 'cat4',
+          'id' :8,
           'title' : 'Category 4.1',
           'image' : 'assets/img/speakers/eagle.jpg',
-          'description' : "Upload 5 hour age by mgs"
+          'description' : "5 hour age",
+          'count' : 589,
+          'owner' : 'mgs',
+          'imageOwner' : 'assets/img/speakers/steve.jpg'
         },
         {
+          'category' : 'cat4',
+          'id' :9,
           'title' : 'Category 4.2',
           'image' : 'assets/img/speakers/cheetah.jpg',
-          'description' : "Upload yesterday by mgs",
-          'count' :32
+          'description' : "yesterday",
+          'count' :32,
+          'owner' : 'mgs',
+          'imageOwner' : 'assets/img/speakers/steve.jpg'
         }
       ]
-    }
+    };
     this.selectedCat = this.json[data];
   }
+  getData(data){
+    console.log(data)
+    this.endLevelDataStatus = true;
+    this.endLevelData = data;
+
+    console.log("this.endLevelDataStatus : ", this.endLevelDataStatus)
+  }
+
 }
